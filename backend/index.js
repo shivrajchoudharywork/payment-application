94602238070de1b31d845f874ce20094d442ee61
shivrajchoudharywork <shivrajchoudharywork@gmail.com>
2024-07-23 23:08:36 +0530

    add user routes

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,7 @@
-const express = require("express");
+import express from "express";
+const app = express();
+import { router } from "./routes/index.js"
 
 
+
+app.use("/api/v1", router)
---
+++ backend/routes/index.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import {userRouter } from "./user.js"
+
+const router = Router();
+
+router.use("/user", userRouter)
+
+module.exports({
+  router
+})
